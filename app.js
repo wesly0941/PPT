9b494ff031a6048e93de9fb4b6f90a2c642d3c1a
wesly0941 <vielmawesly@gmail.com>
2025-10-21 00:16:03 -0400

    JS v2f

--- app.js
+++ app.js
@@ -72,5 +72,56 @@ function game_vs(select_player1, select_player2) {
             vs = "p1win";
         } 
     }
+    combate(); 
 
-}
+        if (vs == "p1win") {
+            document.getElementById("result").innerText = "JUGADOR GANA";
+            document.getElementById("result").style.color = "#00ff0dff";
+            ptos_player1++;
+            document.getElementById("ptos_player1").innerText = ptos_player1;
+        } else if (vs == "same") {
+            document.getElementById("result").innerText = "EMPATE";
+            document.getElementById("result").style.color = "#ffbb00ff";
+        } else if (vs == "p2win") {
+            document.getElementById("result").innerText = "CPU GANA";
+            document.getElementById("result").style.color = "#ff0000ff";
+            ptos_player2++;
+            document.getElementById("ptos_player2").innerText = ptos_player2;
+        }
+    }
+
+    function display_option_player2(select_player2) {
+        if (select_player2 == "piedra") {
+            img_opt_player2.src = "./css/piedra.png";
+        } else if (select_player2 == "papel") {
+            img_opt_player2.src = "./css/papel.png";
+        } else {
+            img_opt_player2.src = "./css/tijeras.png";
+        }
+    }
+
+    const reload = document.getElementById('reset');
+    reload.addEventListener('click', _ => {
+        location.reload();
+    });
+
+    function Crearmensaje(result) {
+        let sectionmensaje = document.getElementById('historico');
+        let parrafo = document.createElement('p');
+        parrafo.textContent = 'Jugador eligió ' + select_player1 + ' y CPU eligió ' + select_player2 + ' - ' + result;
+        sectionmensaje.appendChild(parrafo); 
+    }
+
+    function combate() {
+        if (select_player1 == select_player2) {
+            Crearmensaje("EMPATE");
+        } else if (select_player1 == 'piedra' && select_player2 == 'tijera') {
+            Crearmensaje("GANASTE");
+        } else if (select_player1 == 'tijeras' && select_player2 == 'papel') {
+            Crearmensaje("GANASTE");
+        } else if (select_player1 == 'papel' && select_player2 == 'piedra') {
+            Crearmensaje("GANASTE");
+        } else {
+            Crearmensaje("PERDISTE");
+        }
+    }
